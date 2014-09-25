a87bf5283b4135a240677223876e64e38d2639e8
GongPA <david.chen@spsmail.cuny.edu>
2014-09-25 07:44:47 -0400

    more doing more learning

--- task_03.py
+++ task_03.py
@@ -1,22 +1,27 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 """redo task 3 4 5"""
-BASE = raw_input(
-"Please pick between 2 colors \n "
-"Seattle Gray or Manatee? : ")
+BASE = raw_input("Please pick between 2 colors \n"
+                 "Seattle Gray or Manatee? : ")
 
 if BASE == "Seattle Gray":
-    ACCENT = raw_input("Please pick between 2 colors \nCeramic Glaze or Cumulus Nimbus : ")
+    ACCENT = raw_input("Please pick between 2 colors \n"
+                       "Ceramic Glaze or Cumulus Nimbus : ")
     if ACCENT == "Ceramic Glaze":
-        HIGHLIGHT = raw_input("Please pick between 2 colors \nBasically White or White : ")
+        HIGHLIGHT = raw_input("Please pick between 2 colors \n"
+                              "Basically White or White : ")
     elif ACCENT == "Cumulus Nimbus":
-        HIGHLIGHT = raw_input("Please pick between 2 colors \nOff-White or Paper White : ")
+        HIGHLIGHT = raw_input("Please pick between 2 colors \n"
+                              "Off-White or Paper White : ")
 elif BASE == "Manatee":
-    ACCENT = raw_input("Please pick between 2 colors \nPlatinum Mist of Spartan Sage : ")
+    ACCENT = raw_input("Please pick between 2 colors \n"
+                       "Platinum Mist of Spartan Sage : ")
     if ACCENT == "Platinum Mist":
-        HIGHLIGHT = raw_input("Please pick between 2 colors \nBone White or Just White : ")
+        HIGHLIGHT = raw_input("Please pick between 2 colors \n"
+                              "Bone White or Just White : ")
     elif ACCENT == "Spartan Sage":
-        HIGHLIGHT = raw_input("Please pick between 2 colors \nFractal White or Not White : ")
+        HIGHLIGHT = raw_input("Please pick between 2 colors \n"
+                              "Fractal White or Not White : ")
 print (
 'Your base color is {0}, your accent is {1}, '
 'and your highlight is {2}.').format(BASE, ACCENT, HIGHLIGHT)
--- task_05.py
+++ task_05.py
@@ -3,8 +3,7 @@
 """redo Task 5"""
 from decimal import Decimal
 NAME = raw_input("What is your name? ").title()
-PRINCIPLE = int(raw_input(
-'What is the amount of your principle (amount being borrowed)? '))
+PRINCIPLE = int(raw_input("What is the amount of your principle (amount being borrowed)? "))
 YEARS = int(raw_input("For how many years is this loan being borrowed? "))
 QUALIFIED = raw_input("Are you prequalified for this loan? ").upper()[:1]
 INTEREST = 0
@@ -13,23 +12,23 @@ if 0 <= PRINCIPLE <= 199999:
         if QUALIFIED == "Y":
             INTEREST = '.0363'
         elif QUALIFIED == "N":
-                INTEREST = '.0465'
+            INTEREST = '.0465'
         else:
-                    INTEREST = '0.0'
+            INTEREST = '0.0'
     elif 15 < YEARS <= 20:
-            if QUALIFIED == "Y":
-                INTEREST = '.0404'
-            elif QUALIFIED == "N":
-                INTEREST = '.0498'  
-            else:
-                INTEREST = '0.0'
+        if QUALIFIED == "Y":
+            INTEREST = '.0404'
+        elif QUALIFIED == "N":
+            INTEREST = '.0498'  
+        else:
+            INTEREST = '0.0'
     elif 20 < YEARS <= 30:
-            if QUALIFIED == "Y":
-                INTEREST = '.0577'
-            elif QUALIFIED == "N":
-                INTEREST = '.0639'
-            else:
-                INTEREST = '0.0'
+        if QUALIFIED == "Y":
+            INTEREST = '.0577'
+        elif QUALIFIED == "N":
+            INTEREST = '.0639'
+        else:
+            INTEREST = '0.0'
 elif 200000 <= PRINCIPLE <= 999999:
     if 1 <= YEARS <= 15:
         if QUALIFIED == "Y":
@@ -63,23 +62,23 @@ PY = Decimal(12)
 TOTAL = Decimal(PRINCIPLE * (1+(INTEREST/PY)) ** (PY * YEARS))
 TOTAL = round(TOTAL)
 if INTEREST == float('0.0'):
-   TOT_REPO = None
-   TOTAL = None
+    TOT_REPO = None
+    TOTAL = None
 else:
-   TOT_REPO = str('{0}{1:,.0f}'.format('$', TOTAL))
-   PRIN_REPO = '{0}{1:,.0f}'.format('$', PRINCIPLE)
-   YEAR_REPO = str(YEARS) + "yrs"
-   if QUALIFIED == 'Y':
+    TOT_REPO = str('{0}{1:,.0f}'.format('$', TOTAL))
+    PRIN_REPO = '{0}{1:,.0f}'.format('$', PRINCIPLE)
+    YEAR_REPO = str(YEARS) + "yrs"
+    if QUALIFIED == 'Y':
         QUALIFIED = 'Yes'
-   else:
+    else:
         QUALIFIED = 'No'
-REPORT = (
-        'Loan Report for: {0}'
-        '\n{1}'
-        '\n\tPrincipal:      {2:>10}'
-        '\n\tDuration:       {3:>10}'
-        '\n\tPre-Qualified?: {4:>10}'
-        '\n'
-        '\n\tTotal:          {5:>10}'
-        ).format(NAME, '-' * 68, PRIN_REPO, YEAR_REPO, QUALIFIED, TOT_REPO)
+REPORT = ('Loan Report for: {0}'
+          '\n{1}'
+          '\n\tPrincipal:      {2:>10}'
+          '\n\tDuration:       {3:>10}'
+          '\n\tPre-Qualified?: {4:>10}'
+          '\n'
+          '\n\tTotal:          {5:>10}').format(NAME, '-' * 68,
+                                                PRIN_REPO, YEAR_REPO,
+                                                QUALIFIED, TOT_REPO)
 print REPORT
