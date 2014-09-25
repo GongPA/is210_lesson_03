4c373e7a8b44a13926ab90e57d86ead631f56c5f
GongPA <david.chen@spsmail.cuny.edu>
2014-09-25 07:31:26 -0400

    more doing more learning

--- task_03.py
+++ task_03.py
@@ -2,20 +2,21 @@
 # -*- coding: utf-8 -*-
 """redo task 3 4 5"""
 BASE = raw_input(
-"Please pick between 2 colors - "
+"Please pick between 2 colors \n "
 "Seattle Gray or Manatee? : ")
+
 if BASE == "Seattle Gray":
-    ACCENT = raw_input("Ceramic Glaze or Cumulus Nimbus : ")
-if ACCENT == "Ceramic Glaze":
-    HIGHLIGHT = raw_input("Basically White or White : ")
-elif ACCENT == "Cumulus Nimbus":
-    HIGHLIGHT = raw_input("Off-White or Paper White : ")
+    ACCENT = raw_input("Please pick between 2 colors \nCeramic Glaze or Cumulus Nimbus : ")
+    if ACCENT == "Ceramic Glaze":
+        HIGHLIGHT = raw_input("Please pick between 2 colors \nBasically White or White : ")
+    elif ACCENT == "Cumulus Nimbus":
+        HIGHLIGHT = raw_input("Please pick between 2 colors \nOff-White or Paper White : ")
 elif BASE == "Manatee":
-    ACCENT = raw_input("Platinum Mist of Spartan Sage : ")
-if ACCENT == "Platinum Mist":
-    HIGHLIGHT = raw_input("Bone White or Just White : ")
-elif ACCENT == "Spartan Sage":
-    HIGHLIGHT = raw_input("Fractal White or Not White : ")
+    ACCENT = raw_input("Please pick between 2 colors \nPlatinum Mist of Spartan Sage : ")
+    if ACCENT == "Platinum Mist":
+        HIGHLIGHT = raw_input("Please pick between 2 colors \nBone White or Just White : ")
+    elif ACCENT == "Spartan Sage":
+        HIGHLIGHT = raw_input("Please pick between 2 colors \nFractal White or Not White : ")
 print (
 'Your base color is {0}, your accent is {1}, '
 'and your highlight is {2}.').format(BASE, ACCENT, HIGHLIGHT)
