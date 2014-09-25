306e6ebe6755e3dfa74f4a540d853fa01a0daf6e
GongPA <david.chen@spsmail.cuny.edu>
2014-09-25 07:20:48 -0400

    more doing more learning

--- task_03.py
+++ task_03.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-"""redo Task 3"""
+"""redo task 3 4 5"""
 BASE = raw_input(
-"Please pick between 2 colors \n "
+"Please pick between 2 colors - "
 "Seattle Gray or Manatee? : ")
 if BASE == "Seattle Gray":
     ACCENT = raw_input("Ceramic Glaze or Cumulus Nimbus : ")
@@ -16,5 +16,6 @@ if ACCENT == "Platinum Mist":
     HIGHLIGHT = raw_input("Bone White or Just White : ")
 elif ACCENT == "Spartan Sage":
     HIGHLIGHT = raw_input("Fractal White or Not White : ")
-print ("Your base color is {0}, your accent is {1}, and your highlight is {2}."
-).format(BASE, ACCENT, HIGHLIGHT)
+print (
+'Your base color is {0}, your accent is {1}, '
+'and your highlight is {2}.').format(BASE, ACCENT, HIGHLIGHT)
